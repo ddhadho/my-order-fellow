2b770c1fa90499cf218dbbf846c8bec0ad90f9e9
dhadho <dhaddho24@gmail.com>
2026-02-01 12:35:28 +0300

    feat(app): register orders module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { KycModule } from './modules/kyc/kyc.module';
 import { WebhooksModule } from './modules/webhooks/webhooks.module'; 
+import { NotificationsModule } from './modules/notifications/notifications.module';
 import { OrdersModule } from './modules/orders/orders.module';
 import configuration from './config/configuration';
 
@@ -24,6 +25,8 @@ import configuration from './config/configuration';
     AuthModule,
     KycModule,
     WebhooksModule,
+    NotificationsModule,
+    OrdersModule,
   ],
 })
 export class AppModule {}
